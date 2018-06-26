85112306310df6e8e21d52bfaedf0713e19e06a7
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-06-26 11:14:32 +0530

    home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,10 +6,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  likes: number = 1000;
+  // type inference
+  price = 99.99
+
+  productName = "Google Pixel"
+
+  // address // => any, bad practice
+
 
   constructor() { }
 
   ngOnInit() {
+    //FIXME
+    setTimeout( ()=> {
+      console.trace()
+      console.log("timeout called")
+      this.likes += 100
+    }, 5000)
+  }
+
+  divClick() {
+    alert('div click')
+  }
+
+  increment(e: Event) {
+
+    console.trace()
+
+    e.stopPropagation()
+    
+    console.log("Event ", e)
+    this.likes++;
+
+    alert('increment')
   }
 
 }
