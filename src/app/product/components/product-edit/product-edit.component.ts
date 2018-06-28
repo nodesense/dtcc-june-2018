db6c647bfd7bb06c10e1fd6852a5d9cdcc7de331
Gopalakrishnan Subramani <gopalakrishnan.subramani@gmail.com>
2018-06-28 12:13:35 +0530

    form starter

--- src/app/product/components/product-edit/product-edit.component.ts
+++ src/app/product/components/product-edit/product-edit.component.ts
@@ -1,4 +1,12 @@
+import { Brand } from './../../models/brand';
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
+
+import {ActivatedRoute,  // read url data like :id
+        Router // navigate one page to another page
+       } from '@angular/router'
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product-edit',
@@ -7,9 +15,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductEditComponent implements OnInit {
 
-  constructor() { }
+  product: Product = new Product(); //for create page
+  brands$:Observable<Brand[]>;
+
+  constructor(private route: ActivatedRoute,
+               private router: Router, 
+              private productService: ProductService) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params['id']; 
+    console.log("ID ", id)
+
+    if (id) {
+      this.productService.getProduct(id)
+          .subscribe( product => {
+            this.product = product;
+          })
+    }
+
+
+    this.brands$ = this.productService.getBrands()
+
   }
 
 }
